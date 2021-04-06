dbb22e20e663ecef993c10feca49ada762540ec2
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-05 22:32:40 -0500

    Comprobación de correo en auth

--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,35 @@
 const { response } = require('Express');
+const User = require('../models/user');
+
+const login = async ( req, res = response ) =>  {
 
 
-const login = ( req, res = response ) =>  {
-
-    res.json({
-        msg: 'login'
-    });
+    const { userEmail, userPassword } = req.body;
 
 
+    try{
+
+        //Verificar si usuario existe 
+        const user =  await User.findOne( { userEmail } );
+
+        if( !user ){
+            return res.status(400).json({
+                msg: 'Usario / password no son correcto -correo'
+            });
+        }
+
+        res.json({
+        msg: 'login',
+        
+
+        });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Algo salio mal, hable con el administrador'
+        });
+    }
 }
 
 
