c4cb83e195b6a1c3c9bda14798dda9770176604b
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-05 22:12:15 -0500

    validation auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,19 @@
 const { Router } = require( 'express' );
 const { check } = require('express-validator');
 
+
+const { validarCampos } = require('../middlewares/validar.js');
+
 const { login } = require('../controller/auth');
 
 
 const router = Router();
 
-    router.post( '/login', login  );
+    router.post( '/login', [
+        check( 'userEmail', 'El correo es obligatorio' ).isEmail(),
+        check('userPassword', 'El password es obligatorio').not().isEmpty(),
+        validarCampos
+    ] ,  login  );
 
 
 
