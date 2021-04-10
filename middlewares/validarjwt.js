776b06033c4145cd3bc63543109e2ac5b2e4ab75
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-09 21:09:15 -0500

    match with token

--- middlewares/validarjwt.js
+++ middlewares/validarjwt.js
@@ -6,9 +6,28 @@ const validarJWT = (    req = request ,  res = response, next  ) => {
     
     const token = req.header( 'x-token' );
 
+    if( !token ){
+        return res.status(401).json({
+            msg: 'No hay Token en la peticion'
+        });
+    }
+
+
+    try{
+
+        jwt.verify( token, process.env.SECRETORPRIVATEKEY );
+
+        next();
+    }catch( err ){
+        console.log( err );
+        res.status(401).json({
+            msg: 'Token no valido'
+        })
+    }
+
     console.log( token );
 
-    next();
+
 
 }
 
