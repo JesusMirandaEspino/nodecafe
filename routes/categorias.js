8a23d604b2329fca6ca447c14bff47adb79a507d
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-21 21:34:30 -0500

    router of categories

--- routes/categorias.js
+++ routes/categorias.js
@@ -6,12 +6,41 @@ const { validarCampos } = require('../middlewares/validar.js');
 const router = Router();
 
 
+// Obtener todas las categorias - publico
     router.get('/', ( req, res ) => {
         res.json({
-            msg: 'Todo ok'
+            msg: 'get'
         });
     });
 
+// Obtener una categoria por id - publico
+    router.get('/:id', ( req, res ) => {
+        res.json({
+            msg: 'get - id'
+        });
+    });
+
+
+// Crear una categoria - privado cualquier persona con un token valido
+    router.post('/', ( req, res ) => {
+        res.json({
+            msg: 'Post'
+        });
+    });
+
+// Actualizar la categoria - privado cualquier persona con un token valido
+    router.put('/:id', ( req, res ) => {
+        res.json({
+            msg: 'Put'
+        });
+    });
+
+// Borrar categoria Admin
+    router.delete('/:id', ( req, res ) => {
+        res.json({
+            msg: 'Desactivar'
+        });
+    });
 
 
 module.exports = router; 
