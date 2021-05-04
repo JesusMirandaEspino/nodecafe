a015f8e944ac80d47c483f2fea63c17856b6ee7b
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-03 22:44:38 -0500

    busqueda basica

--- controller/buscar.js
+++ controller/buscar.js
@@ -1,9 +1,52 @@
 const { response }  = require('express');
 
 
+const coleccionesPermitidas = [
+    'user', 
+    'category',
+    'product',
+    'roles'
+
+];
+
+
 const buscar = ( req, res = reponse ) => {
+
+    const { coleccion, termino  }  = req.params;
+
+
+    if( !coleccionesPermitidas.includes(coleccion) ){
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son ${ coleccionesPermitidas }`
+        });
+    }
+
+
+    switch( key ){
+
+        case  'user':
+
+        break;
+
+        case 'category':
+
+        break;
+
+        case 'product':
+
+        break;
+
+        default: 
+        res.status(500).json({
+            msg: `Se le olvido hacer esta busqueda`
+        });
+    }
+
+
+
     res.json({
-        msg: 'Buscar'
+        coleccion, 
+        termino
     });
 }
 
