41ec8bef71da71de93c5757bc202a729a33d9805
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-07 21:56:29 -0500

    id for archive with uuid

--- controller/uploads.js
+++ controller/uploads.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 
 const { response } = require("express");
@@ -14,24 +13,21 @@ const cargarArchivo =  ( req, res = response ) => {
 
     console.log( req.files.archivo );
 
-
-
-    
-
+    // validar si se envio un archivo
     if (!req.files || Object.keys(req.files).length === 0  || !req.files.archivo ) {
         res.status(400).json({ msg: 'No hay archivo para enviar' });
         return;
     }
 
+
+    //obtener extension del archivo
     const { archivo } = req.files;
     const shortname = req.files.archivo.name.split('.'); 
     const extension = shortname[ shortname.length  - 1 ];
 
 
     //validar extension
-    const extensionesValidas = [ 'jpg', 'png', 'jpeg', 'gif' ];
-
-
+    const extensionesValidas = [ 'JPG', 'PNG', 'JPEG', 'GIF' ];
     if( !extensionesValidas.includes(extension) ){
         return res.status( 400 ).json( {
             msg: `La extension ${ extension } no es el tipo valida, Las permitidas son ${extensionesValidas}`
@@ -39,10 +35,9 @@ const cargarArchivo =  ( req, res = response ) => {
     }
 
 
-
-
-    /*
-    const uploadPath = path.join(  __dirname, '../uploads/',  archivo.name) ;
+    //mover arhivo a carpeta especifica
+    const nameTemp = uuidv4() + '.' + extension;
+    const uploadPath = path.join(  __dirname, '../uploads/',  nameTemp ) ;
 
     archivo.mv(uploadPath, (err) =>  {
 
@@ -53,7 +48,7 @@ const cargarArchivo =  ( req, res = response ) => {
     res.json( { msg: 'File uploaded to ' + uploadPath });
     });
 
-*/
+
 
 
 } 
