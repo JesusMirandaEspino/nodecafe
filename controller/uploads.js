26f92dcf32894b63ed3d8d3a39ba95a8efa301d8
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-07 21:24:12 -0500

    right send error headers

--- controller/uploads.js
+++ controller/uploads.js
@@ -1,8 +1,37 @@
+const path = require('path');
+
+
 const { response } = require("express");
 
 
 
-const cargarArchivo = ( req, res = response ) => {
+
+const cargarArchivo = async ( req, res = response ) => {
+
+
+    console.log( req.files.archivo );
+    
+
+    if (!req.files || Object.keys(req.files).length === 0  || !req.files.archivo ) {
+        res.status(400).json({ msg: 'No hay archivo para enviar' });
+        return;
+    }
+
+    const { archivo } = req.files;
+
+    const uploadPath = path.join(  __dirname, '../uploads/',  archivo.name) ;
+
+    await archivo.mv(uploadPath, (err) =>  {
+
+        if (err) {
+        return res.status(500).json({err});
+        }
+
+    res.json( { msg: 'File uploaded to ' + uploadPath });
+    });
+
+
+
 
     res.json({
         msg: 'Hola mundo'
