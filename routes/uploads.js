e29342b838c1a9aacbb96e50f71b56edcc843870
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-12 21:49:04 -0500

    route validator

--- routes/uploads.js
+++ routes/uploads.js
@@ -12,7 +12,11 @@ const router = Router();
 
     router.post( '/', cargarArchivo );
 
-    router.put( '/:coleccion/:id', [], actualizarImagen );
+    router.put( '/:coleccion/:id', [ 
+        check( 'id', 'El id debe ser de mongo' ).isMongoId(),
+        check( 'coleccion' ).custom( c => coleccionesPermitidas( c, [ 'users', 'products'  ] ) ),
+        validarCampos
+    ], actualizarImagen );
 
 
 
